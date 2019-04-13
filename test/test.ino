0d2273294c18940d57e5d05ae86d81efdc440fb0
Åsmund Runningen <be20080203@vgs.bfk.no>
2019-04-13 16:54:26 +0200

    skrev om test og test2 for å se om funksjonen tid=endring^0.5/2*1000 er bedre for å endre volumet

--- test/test.ino
+++ test/test.ino
@@ -1,155 +1,74 @@
-#define volumeDown 2
-#define volumeUp 3
-#define echoPin 8
-#define trigerPin 7
-int const playPin = 4;
-boolean appConnected = false;
 
-int volume = 100; //volumet i prosent
-int newVolume = 100;
+//definering av forskjellige porter sånn at vi kan sikte til de senere i koden uten at de må ta opp minne på arduinoen.
+#define volumeDown 5 //port for å skru volumet på Bluethootkortet ned
+#define volumeUp 6 //port for å skru volumet på Bluethootkortet opp
+#define volume100 3
+#define volume50 4
+#define volume0 5
 
-boolean paused = false;
 
-float distance[11];
-float averageDistance;
-float currentDistance;
-int const maxDistance = 100;
-int const minDistance = 10;
+int volume = 100; //faktiske volumet i prosent
+int newVolume = 100; //verdi for hva volumet skal endre seg til
 
-int const changeVolumeSensitivity = 1;
-float originalVolume;
-
-float handInside[] = {0, 0};
-
-float volumeChangeStartTime;
+float volumeChangeStartTime; //forteller når man startet å sende et signal til ultralydsensoren for å justere volumet
 
 
 void setup(){
   Serial.begin(9600);
   pinMode(volumeDown, OUTPUT);
   pinMode(volumeUp, OUTPUT);
-
-  pinMode(echoPin, INPUT);
-  pinMode(trigerPin, OUTPUT);
+  pinMode(volume100, OUTPUT);
+  pinMode(volume50, OUTPUT);
+  pinMode(volume0, OUTPUT);
   }
 
 void loop(){
 
-  /*justering av volumet på Bluethootkortet
-  -----------------------------------------
-  -----------------------------------------
-  -----------------------------------------*/
-  if (appConnected == false){
+  /*justering av volum
+  --------------------
+  --------------------*/
+  //justering av volumet på Bluethootkortet
   if (volume != newVolume && volumeChangeStartTime == 0){
     volumeChangeStartTime = millis();} //starter en klokke som forteller når vi startet å endre volumet
 
+  //volumet er over det ønskede volumet så det må justeres ned
   if (newVolume < volume){
     digitalWrite(volumeUp, LOW);
     digitalWrite(volumeDown, HIGH);
-    if ((volume - newVolume)*50 < (millis())-volumeChangeStartTime){ //hvis endringen i volum er ferdig
+    if (((volume - newVolume)^0.5/2)*1000 < (millis())-volumeChangeStartTime){ //beregner om singalet er sendt lenge nok til at den er ferdig med endringne i volumet
       volume = newVolume;
       digitalWrite(volumeDown, LOW);
-      volumeChangeStartTime = 0;
+      volumeChangeStartTime = 0; //oppdaterer volumet til nodemcuen.
     }
     }
-
+  //volumet er under det ønskede volumet så det må justeres oppover
   else if (newVolume > volume){
     digitalWrite(volumeDown, LOW);
     digitalWrite(volumeUp, HIGH);
-    if ((newVolume - volume)*50 < (millis())-volumeChangeStartTime){ //hvis endringen i volum er ferdig
+    if (((newVolume - volume)^0.5/2)*1000 < (millis())-volumeChangeStartTime){ //beregner om singalet er sendt lenge nok til at den er ferdig med endringne i volumet
       volume = newVolume;
       if (newVolume == 100){}
       digitalWrite(volumeUp, LOW);
-      volumeChangeStartTime = 0;}}}
+      volumeChangeStartTime = 0;}}
 
-    /*håndtering av kommunikasjon med Nodemcuen
-    -------------------------------------------
-    -------------------------------------------
-    -------------------------------------------*/
-    if (Serial.available() > 0){
 
+    if (newVolume == 100){
+      digitalWrite(volume100, HIGH);
+      digitalWrite(volume50, LOW);
+      digitalWrite(volume0, LOW);
+      if (volume == 100){volume=0;}
+    }
+    else if (newVolume == 50){
+      digitalWrite(volume50, HIGH);
+      digitalWrite(volume100, LOW);
+      digitalWrite(volume0, LOW);
+      if (volume == 50){volume=100;}
+    }
+    else if (newVolume == 0){
+      digitalWrite(volume0, HIGH);
+      digitalWrite(volume50, LOW);
+      digitalWrite(volume100, LOW);
+      if (volume == 0){volume=50;}
     }
 
-
-    /*testing
-    ----------------------------------------
-    ----------------------------------------
-    ----------------------------------------*/
-    Serial.print(newVolume);
-    Serial.print("    ");
-    Serial.println(volume);
-
-
-
-
-    /*bruk av ultralydsensoren for å finne endringen i volumet
-    --------------------------------------------
-    --------------------------------------------
-    */
-
-    //beregner avstanden som ultralydsensoren registrerer
-      digitalWrite(trigerPin, LOW);
-      delayMicroseconds(2);
-      digitalWrite(trigerPin, HIGH);
-      delay(10);
-      digitalWrite(trigerPin, LOW);
-      currentDistance = pulseIn(echoPin, HIGH)*0.034/2;
-
-    //sørger for at man bare lagrer verdien hvis den er innenfor maxverdien
-    if (currentDistance < maxDistance){
-      distance[int(distance[0])] = currentDistance;
-    }
-    else{
-      distance[int(distance[0])] = 0;}
-
-    //justerer distance[0] som er hvilken verdi som er den eldste
-    if (distance[0] < 10){
-      distance[0]  ++;}
-    else{
-    distance[0] = 1;}
-
-    //regner ut gjennomsnittet av verdiene lagret i distance
-    {float sum = 0;
-    int numberOfReadings = 0;
-    for (int i = 1; i<11; i++){
-      //regner ut gjennomsnittsavstanden
-        //sikrer at man bare regner ut gjennomsnittet av de verdiene som er innenfor maksverdien
-        if (distance[i] != 0){
-          sum += distance[i];
-          numberOfReadings ++;}
-        //tar gjennomsnittet av verdiene som er lagt sammen i forige if statement
-        if (i == 10){
-          averageDistance = sum/numberOfReadings;}}
-
-    //Hvis det er færre en 5 verdier som er innenfor maksverdien skal det regnes som at hånden er utenfor lydsensoren
-    if (numberOfReadings < 5){
-      handInside[0] = 0;
-      handInside[1] = 0;
-      originalVolume = 0;}
-    else if (handInside[0] == 0){
-      handInside[0] = 1;
-      handInside[1] = averageDistance;
-      originalVolume = newVolume;}
-    }
-    //regner ut endringen i newVolumet
-    if (handInside[0] == 1){
-       //sikrer at den ikke pauser sangen mer en en gang når hånden er innefor området og at den ikke endrer på volumet når sangen er blitt pauset hvis man flytter hånden
-      if (averageDistance < 5 && paused != true){ //hvis hånden er innenfor 5 cm skal den pause/starte musikken istedenfor å justere volumet.        digitalWrite(playPin, HIGH);
-        Serial.println("pausing/starting phone");
-        digitalWrite(playPin, HIGH);
-        delay(400); //Bluethootkortet bruker ca 105 ms på å registrere at musikken pauses.
-        digitalWrite(playPin, LOW);
-        paused = true;
-      }else{
-        newVolume = originalVolume + (averageDistance - handInside[1])*changeVolumeSensitivity;}}
-
-      else{paused = false;} //sørger for at den ikke starter og stopper musikken flere ganger når man tar hånden over ultralydsensoren
-
-
-
-      //sørger for at newVolumet ikke går utenfor 0-100
-      if (newVolume > 100){
-        newVolume = 100;}
-      else if (newVolume < 0){
-        newVolume = 0;}
-        }
+    } //oppadeter volumet på nodemcuen
--- upload/upload.ino
+++ upload/upload.ino
@@ -1,83 +1,71 @@
- // --------------------------------------
-// i2c_scanner
-//
-// Version 1
-//    This program (or code that looks like it)
-//    can be found in many places.
-//    For example on the Arduino.cc forum.
-//    The original author is not know.
-// Version 2, Juni 2012, Using Arduino 1.0.1
-//     Adapted to be as simple as possible by Arduino.cc user Krodal
-// Version 3, Feb 26  2013
-//    V3 by louarnold
-// Version 4, March 3, 2013, Using Arduino 1.0.3
-//    by Arduino.cc user Krodal.
-//    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
-//    according to the i2c scanner by Nick Gammon
-//    https://www.gammon.com.au/forum/?id=10896
-// Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
-// Version 6, November 27, 2015.
-//    Added waiting for the Leonardo serial communication.
-// 
-//
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
-//
 
-#include <Wire.h>
+//definering av forskjellige porter sånn at vi kan sikte til de senere i koden uten at de må ta opp minne på arduinoen.
+#define volumeDown 5 //port for å skru volumet på Bluethootkortet ned
+#define volumeUp 6 //port for å skru volumet på Bluethootkortet opp
+#define volume100 3
+#define volume50 4
+#define volume0 5
 
 
-void setup()
-{
-  Wire.begin();
+int volume = 100; //faktiske volumet i prosent
+int newVolume = 100; //verdi for hva volumet skal endre seg til
 
+float volumeChangeStartTime; //forteller når man startet å sende et signal til ultralydsensoren for å justere volumet
+
+
+void setup(){
   Serial.begin(9600);
-  while (!Serial);             // Leonardo: wait for serial monitor
-  Serial.println("\nI2C Scanner");
-}
-
-
-void loop()
-{
-  byte error, address;
-  int nDevices;
-
-  Serial.println("Scanning...");
-
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) 
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
-
-      nDevices++;
-    }
-    else if (error==4) 
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+  pinMode(volumeDown, OUTPUT);
+  pinMode(volumeUp, OUTPUT);
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
 
-  delay(5000);           // wait 5 seconds for next scan
-}
+void loop(){
+
+  /*justering av volum
+  --------------------
+  --------------------*/
+  //justering av volumet på Bluethootkortet
+  if (volume != newVolume && volumeChangeStartTime == 0){
+    volumeChangeStartTime = millis();} //starter en klokke som forteller når vi startet å endre volumet
+
+  //volumet er over det ønskede volumet så det må justeres ned
+  if (newVolume < volume){
+    digitalWrite(volumeUp, LOW);
+    digitalWrite(volumeDown, HIGH);
+    if ((volume - newVolume)^0.5/2 < (millis())-volumeChangeStartTime){ //beregner om singalet er sendt lenge nok til at den er ferdig med endringne i volumet
+      volume = newVolume;
+      digitalWrite(volumeDown, LOW);
+      volumeChangeStartTime = 0; //oppdaterer volumet til nodemcuen.
+    }
+    }
+  //volumet er under det ønskede volumet så det må justeres oppover
+  else if (newVolume > volume){
+    digitalWrite(volumeDown, LOW);
+    digitalWrite(volumeUp, HIGH);
+    if ((newVolume - volume)^0.5/2 < (millis())-volumeChangeStartTime){ //beregner om singalet er sendt lenge nok til at den er ferdig med endringne i volumet
+      volume = newVolume;
+      if (newVolume == 100){}
+      digitalWrite(volumeUp, LOW);
+      volumeChangeStartTime = 0;}}
+
+
+    if (newVolume == 100){
+      digitalWrite(volume100, HIGH);
+      digitalWrite(volume50, LOW);
+      digitalWrite(volume0, LOW);
+      if (volume == 100){volume=0;}
+    }
+    else if (newVolume == 50){
+      digitalWrite(volume50, HIGH);
+      digitalWrite(volume100, LOW);
+      digitalWrite(volume0, LOW);
+      if (volume == 50){volume=100;}
+    }
+    else if (newVolume == 0){
+      digitalWrite(volume0, HIGH);
+      digitalWrite(volume50, LOW);
+      digitalWrite(volume100, LOW);
+      if (volume == 0){volume=50;}
+    }
+
+    } //oppadeter volumet på nodemcuen
